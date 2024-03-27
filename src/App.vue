845e23510258dadc7299a33f5310cf1a0463b59a
Pedro Figueroa <59029273+mardelnet@users.noreply.github.com>
2024-03-27 11:25:09 +0100

    Add slider

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,55 @@
 <template>
-<p>{{ data.feed.title.label }}</p>
-<div v-for="x in data.feed.entry">
-  <div>{{ x['im:name']['label'] }}</div>
-  <div>{{ x['summary']['label'] }}</div>
-  <div>{{ x['im:image'][2]['label'] }}</div>
-</div>
+  <div v-if="data && data.feed">
+    <swiper
+      :slides-per-view="3"
+      :space-between="50"
+      @swiper="onSwiper"
+      @slideChange="onSlideChange"
+    >
+      <swiper-slide v-for="podcast in data.feed.entry" :key="podcast.id.attributes['im:id']">
+        <img v-bind:src="podcast['im:image'][2].label" />
+        <div>{{ podcast['im:name'].label }}</div>
+        <div>{{ podcast.summary.label }}</div>
+      </swiper-slide>
+    </swiper>
+  </div>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        data: null,
-      };
-    },
-    created() {
-      this.fetchData()
-    },
-    methods: {
-      async fetchData() {      
-        const response = await fetch("https://itunes.apple.com/us/rss/toppodcasts/limit=10/json")
-        this.data = await response.json()
-        console.log(this.data)
-      },    
-    }
-  };
-</script>
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import 'swiper/css';
 
-<style>
-</style>
+export default {
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  methods: {
+    async fetchData() {      
+      const response = await fetch("https://itunes.apple.com/us/rss/toppodcasts/limit=10/json")
+      this.data = await response.json()
+      console.log(this.data)
+    },    
+  },
+  data() {
+    return {
+      data: null
+    }
+  },
+  created() {
+    this.fetchData()
+  },
+  setup() {
+    const onSwiper = (swiper) => {
+      console.log(swiper);
+    };
+    const onSlideChange = () => {
+      console.log('slide change');
+    };
+    return {
+      onSwiper,
+      onSlideChange,
+    };
+  },
+};
+</script>
