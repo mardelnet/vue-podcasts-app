aadf28d79f7f88e842511549541396c3df6a613d
Pedro Figueroa <59029273+mardelnet@users.noreply.github.com>
2024-03-27 10:34:44 +0100

    Call itunes api basic implementation

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,31 @@
 <template>
-  <h1>Food</h1>
+<p>{{ data.feed.title.label }}</p>
+<div v-for="x in data.feed.entry">
+  <div>{{ x['im:name']['label'] }}</div>
+  <div>{{ x['summary']['label'] }}</div>
+  <div>{{ x['im:image'][2]['label'] }}</div>
+</div>
 </template>
 
-<script></script>
+<script>
+  export default {
+    data() {
+      return {
+        data: null,
+      };
+    },
+    created() {
+      this.fetchData()
+    },
+    methods: {
+      async fetchData() {      
+        const response = await fetch("https://itunes.apple.com/us/rss/toppodcasts/limit=10/json")
+        this.data = await response.json()
+        console.log(this.data)
+      },    
+    }
+  };
+</script>
 
 <style>
 </style>
