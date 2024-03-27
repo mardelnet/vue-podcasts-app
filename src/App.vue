c2c1a443e5b471ac019dcc9f77073e1eacbd0fba
Pedro Figueroa <59029273+mardelnet@users.noreply.github.com>
2024-03-27 11:53:29 +0100

    Add podcast item component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div v-if="data && data.feed">
     <swiper
-      :slides-per-view="3"
-      :space-between="50"
-      @swiper="onSwiper"
-      @slideChange="onSlideChange"
+      v-bind:slides-per-view="5"
+      v-bind:space-between="50"
     >
-      <swiper-slide v-for="podcast in data.feed.entry" :key="podcast.id.attributes['im:id']">
-        <img v-bind:src="podcast['im:image'][2].label" />
-        <div>{{ podcast['im:name'].label }}</div>
-        <div>{{ podcast.summary.label }}</div>
+      <swiper-slide 
+        v-for="podcast in data.feed.entry" 
+        v-bind:key="podcast.id.attributes['im:id']"
+      >
+        <podcast-item 
+          v-bind:podcast-name="podcast['im:name'].label"
+          v-bind:podcast-summary="podcast.summary.label"
+          v-bind:podcast-image-url="podcast['im:image'][2].label"
+        />
       </swiper-slide>
     </swiper>
   </div>
@@ -18,11 +21,13 @@
 <script>
 import { Swiper, SwiperSlide } from 'swiper/vue';
 import 'swiper/css';
+import PodcastItem from './components/PodcastItem.vue';
 
 export default {
   components: {
     Swiper,
     SwiperSlide,
+    'podcast-item': PodcastItem
   },
   methods: {
     async fetchData() {      
@@ -39,17 +44,12 @@ export default {
   created() {
     this.fetchData()
   },
-  setup() {
-    const onSwiper = (swiper) => {
-      console.log(swiper);
-    };
-    const onSlideChange = () => {
-      console.log('slide change');
-    };
-    return {
-      onSwiper,
-      onSlideChange,
-    };
-  },
 };
 </script>
+
+<style>
+  body {
+    color: white;
+    background: black
+  }
+</style>
---
+++ src/components/PodcastItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <img v-bind:src="podcastImageUrl" />
+    <h3>{{ podcastName }}</h3>
+    <p>{{ podcastSummary.substr(0, 50) }}...</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [
+    'podcastName',
+    'podcastSummary',
+    'podcastImageUrl',
+  ]
+};
+</script>
+
+<style scoped>
+    p {
+        color: #bbb;
+    }
+</style>
